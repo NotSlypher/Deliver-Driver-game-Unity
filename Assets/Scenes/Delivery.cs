ab03e34cc17238b3fc10968658348d909076ba0b
NotSlypher <86952522+NotSlypher@users.noreply.github.com>
2022-05-30 13:35:32 +0530

    Delivery pick up Indication

--- Assets/Scenes/Delivery.cs
+++ Assets/Scenes/Delivery.cs
@@ -6,10 +6,28 @@ public class Delivery : MonoBehaviour
 {
     [SerializeField] private float delay = 0.3f;
     private bool hasPackage;
+    public SpriteRenderer spriteRenderer;
+    public Sprite packOn;
+    public Sprite packOff;
+
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>()
+    }
 
     void OnCollisionEnter2D(Collision2D other)
     {
         Debug.Log("Ouch");
+    }
+
+    void change()
+    {
+        spriteRenderer.sprite = packOn;
+    }
+
+    void changeBack()
+    {
+        spriteRenderer.sprite = packOff;
     }
 
     void OnTriggerEnter2D(Collider2D other)
@@ -19,11 +37,13 @@ public class Delivery : MonoBehaviour
             Debug.Log("Package picked up");
             hasPackage = true;
             Destroy(other.gameObject,delay);
+            Invoke("change",delay);
         }
         else if (other.tag == "Customer" && hasPackage)
         {
             Debug.Log("Package delivered");
             hasPackage = false;
+            Invoke("changeBack",delay);
         }
     }
 }
